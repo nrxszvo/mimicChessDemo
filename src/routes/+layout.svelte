683f47912d43936a930407a6279eec519a3eee4a
Michael Horgan <michaelghorgan@gmail.com>
2025-03-20 19:24:45 -0700

    fix bg

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,7 @@
 	let { children }: LayoutProps = $props();
 </script>
 
-<div class="h-screen dark:bg-stone-800 dark:text-gray-200">
+<div class="h-full min-h-screen dark:bg-stone-800 dark:text-gray-200">
 	<div class="flex flex-row items-center justify-around pt-4 pb-2">
 		<NavButton onclick={() => goto('/')}>home</NavButton>
 		<NavButton onclick={() => goto('/about')}>about</NavButton>
